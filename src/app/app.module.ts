225cc90ab4a1fd7f8f58fe7f8070d9f4190e7534
Planck Epoch <munaviruzaman.in@yahoo.com>
2023-01-30 17:11:29 +0530

    third commit color card completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { CardSectionComponent } from './card-section/card-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeroSectionComponent,
+    CardSectionComponent,
   ],
   imports: [
     BrowserModule
